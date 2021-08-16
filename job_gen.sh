26f0a9ca9d559a14a2ef8f39b0acbc5344b3a5c6
yindaheng98 <yindaheng98@163.com>
2021-08-16 11:55:46 +0800

    basic template engine

--- job_gen.sh
+++ job_gen.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -x
-N=$1
+HERE=$(dirname ${BASH_SOURCE[0]})
+
+TEMPLATE=$1
+N=$2
 while true; do
     for GPU in $(lsload | grep gpu | grep ok | awk '{print $1}'); do
-        echo "bsub -q gpu_v100 -m \"$GPU\" -gpu \"num=1\" python main_frogsrn.py --cuda --depth $N"
+        eval "cat <<EOF
+$(<$TEMPLATE)
+EOF" >$HERE/jobs/$N.$(basename $TEMPLATE)
         ((N--))
         if ((N <= 0)); then
             break 2
