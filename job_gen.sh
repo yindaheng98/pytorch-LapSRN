d92824e1727fcb3f25f6d566f211d4f4bc7b2603
yindaheng98 <yindaheng98@163.com>
2021-08-16 12:16:46 +0800

    bug fixed

--- job_gen.sh
+++ job_gen.sh
@@ -6,7 +6,7 @@ mkdir -p "$JOBSDIR"
 TEMPLATE=$1
 N=$2
 while true; do
-    for GPU in $(lsload | grep gpu | grep ok | awk '{print $1}'); do
+    for GPU in $(bhosts | grep gpu | grep ok | awk '{print $1}'); do
         FILE="$JOBSDIR/$N.$(basename $TEMPLATE)"
         eval "cat <<EOF
 $(<$TEMPLATE)
