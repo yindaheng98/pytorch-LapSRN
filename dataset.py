c3d7f2eceae0793449a06f70dc33a4ffbb6a8398
yindaheng98 <yindaheng98@163.com>
2021-08-16 15:38:36 +0800

    file conflict

--- dataset.py
+++ dataset.py
@@ -2,11 +2,17 @@ import torch.utils.data as data
 import torch
 import numpy as np
 import h5py
+import shutil
+import uuid
+import os
 
 class DatasetFromHdf5(data.Dataset):
     def __init__(self, file_path):
         super(DatasetFromHdf5, self).__init__()
-        hf = h5py.File(file_path)
+        os.makedirs('tmp', exist_ok=True)
+        tmp = os.path.join('tmp', str(uuid.uuid1()))
+        shutil.copyfile(file_path, tmp)
+        hf = h5py.File(tmp)
         self.data = hf.get("data")
         self.label_x2 = hf.get("label_x2")
         self.label_x4 = hf.get("label_x4")
--- job_templates/job_frogsrn.sh
+++ job_templates/job_frogsrn.sh
@@ -10,4 +10,5 @@ module load anaconda3
 module load cuda-11.02
 conda activate /seu_share/home/dongfang/df_yindh/frogsrn
 cd $HERE #进入作业工作目录
+mkdir -p log
 python main_frogsrn.py --cuda --depth $N
