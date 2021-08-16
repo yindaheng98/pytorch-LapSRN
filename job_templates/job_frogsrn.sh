231ba8a1cc6e776843ef882a27de8621896e42f0
yindaheng98 <yindaheng98@163.com>
2021-08-16 11:51:53 +0800

    job templates

---
+++ job_templates/job_frogsrn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -J frogsrn-$N #任务名称
+#BSUB -q gpu_v100  #队列名称，可用bqueues查看
+#BSUB -m $GPU      #指定节点
+#BSUB -gpu "num=1" #GPU数
+#BSUB -o $HERE/log/frogsrn-$N.out
+#BSUB -e $HERE/log/frogsrn-$N.err
+export TERM=xterm
+module load anaconda3
+module load cuda-11.02
+conda activate /seu_share/home/dongfang/df_yindh/frogsrn
+cd $HERE #进入作业工作目录
+python main_frogsrn.py --cuda --depth $N
